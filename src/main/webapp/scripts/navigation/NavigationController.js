75d9424186a49b9703198eba2feac21a005a728b
Paris Roman <parisroman@gmail.com>
2015-07-07 18:02:00 -0500

    Add Steps and Nav for WISE

--- src/main/webapp/scripts/navigation/NavigationController.js
+++ src/main/webapp/scripts/navigation/NavigationController.js
@@ -92,17 +92,17 @@ var NavigationController = BaseController.extend({
     stepClass: function(step, stepname){
         var me = this;
 
-        if ((step == '1' && stepname == 'picker') || (step == '2' && stepname == 'mapper') || (step == '3' && stepname == 'schedule')){
+        if ((step == '1' && stepname == 'picker') || (step == '2' && stepname == 'mapper') || (step == '2' && stepname == 'datalist') || (step == '3' && stepname == 'schedule')){
             return 'active'
         }
-        else if ((step == '1' && stepname == 'mapper') || (step == '1' && stepname == 'schedule') || (step == '2' && stepname == 'schedule')){
+        else if ((step == '1' && stepname == 'mapper') || (step == '1' && stepname == 'schedule') || (step == '2' && stepname == 'schedule') || (step == '2' && stepname == 'datalist')){
             return 'completed'
         }
 
     },
 
     showStepTitle: function(step, stepname){
-        if ((step == '1' && stepname == 'picker') || (step == '2' && stepname == 'mapper') || (step == '3' && stepname == 'schedule')){
+        if ((step == '1' && stepname == 'picker') || (step == '2' && stepname == 'mapper') || (step == '2' && stepname == 'datalist') || (step == '3' && stepname == 'schedule')){
             return true
         }
     }
