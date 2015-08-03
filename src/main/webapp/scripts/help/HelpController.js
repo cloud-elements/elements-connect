b08312f651dea14e60518386116d5253d4326ea0
Paris Roman <parisroman@gmail.com>
2015-08-03 12:47:55 -0500

    CE-2102 Bulkloader Help Guide - update numbers CE-2193 Bulkloader mapper layout issues in Firefox

--- src/main/webapp/scripts/help/HelpController.js
+++ src/main/webapp/scripts/help/HelpController.js
@@ -38,32 +38,30 @@ var HelpController = BaseController.extend({
 
         if (me._application.getView() == 'mapper') {
             me.$scope.helpList = [
-                {imageURL: 'help-images/BulkloaderClickThrough-1.png', text:'Select a service, the source of your data..'},
-                {imageURL: 'help-images/BulkloaderClickThrough-2.png', text:'Enter your credentials for that service.'},
-                {imageURL: 'help-images/BulkloaderClickThrough-3.png', text:'Select HubSpot to connect your account.'},
-                {imageURL: 'help-images/BulkloaderClickThrough-4.png', text:'Enter your HubSpot Portal ID.'},
-                {imageURL: 'help-images/BulkloaderClickThrough-5.png', text:'Login to your HubSpot account.'},
-                {imageURL: 'help-images/BulkloaderClickThrough-6.png', text:'Authorize the application.'},
-                {imageURL: 'help-images/BulkloaderClickThrough-7.png', text:'Select an object to map from the source application.'},
-                {imageURL: 'help-images/BulkloaderClickThrough-8.png', text:'Select an object to map form target application.'},
-                {imageURL: 'help-images/BulkloaderClickThrough-9.png', text:'Drag and drop the fields you wish to map from the source to the target.'},
-                {imageURL: 'help-images/BulkloaderClickThrough-10.png', text:'The "X" will delete a field if necessary.'},
-                {imageURL: 'help-images/BulkloaderClickThrough-11.png', text:'Click "Save and Schedule Job".'},
-                {imageURL: 'help-images/BulkloaderClickThrough-12.png', text:'Select the calendar to choose a date.  Data will be pulled from your system starting from this date to the present time.'},
-                {imageURL: 'help-images/BulkloaderClickThrough-13.png', text:'Click "Schedule Job".'},
-                {imageURL: 'help-images/BulkloaderClickThrough-14.png', text:'Click "OK".  An email will notify you when the job has completed.'}
+                {imageURL: 'help-images/BulkloaderClickThrough-1.png', steps:[{text:'1. Select a service, the source of your data.'}]},
+                {imageURL: 'help-images/BulkloaderClickThrough-2.png', steps:[{text:'2. Enter Zoho Username'}, {text:'3. Enter Zoho Password.'}, {text:'4. Click "Create".'}]},
+                {imageURL: 'help-images/BulkloaderClickThrough-3.png', steps:[{text:'5. Select HubSpot to connect your account.'}]},
+                {imageURL: 'help-images/BulkloaderClickThrough-4.png', steps:[{text:'6. Enter your HubSpot Portal ID.'}, {text:'7. Click "Create".'}]},
+                {imageURL: 'help-images/BulkloaderClickThrough-5.png', steps:[{text:'8. Login to your HubSpot Account.'}]},
+                {imageURL: 'help-images/BulkloaderClickThrough-6.png', steps:[{text:'9. Authorize the application.'}]},
+                {imageURL: 'help-images/BulkloaderClickThrough-7.png', steps:[{text:'10. Select an object to map from the source application.'}]},
+                {imageURL: 'help-images/BulkloaderClickThrough-8.png', steps:[{text:'11. Drag and drop the fields you wish to map from the source to the target.'}]},
+                {imageURL: 'help-images/BulkloaderClickThrough-9.png', steps:[{text:'12. The "X" will delete a field if necessary.'}]},
+                {imageURL: 'help-images/BulkloaderClickThrough-10.png', steps:[{text:'13. Click "Save and Schedule Job".'}]},
+                {imageURL: 'help-images/BulkloaderClickThrough-11.png', steps:[{text:'14. Select the calendar to choose a date.  Data will be pulled from your system starting from this date to the present time.'}, {text: '15. Select Date'}, {text:'16. Click "Transfer Now"'}]},
+                {imageURL: 'help-images/BulkloaderClickThrough-12.png', steps:[{text:'17. Click "OK".  An email will notify you when the job has completed.'}]}
             ]
         } else {
             me.$scope.helpList = [
-                {imageURL: 'wise-help-images/BulkloaderClickThrough-01.png', text:'Select a service, the source of your data.'},
-                {imageURL: 'wise-help-images/BulkloaderClickThrough-02.png', text:'Enter your credentials for that service.'},
-                {imageURL: 'wise-help-images/BulkloaderClickThrough-03.png', text:'Select an object to map from the source application.'},
-                {imageURL: 'wise-help-images/BulkloaderClickThrough-04.png', text:'The button at the top will select all fields for the object.'},
-                {imageURL: 'wise-help-images/BulkloaderClickThrough-05.png', text:'Or select the fields you wish to map.'},
-                {imageURL: 'wise-help-images/BulkloaderClickThrough-06.png', text:'Click "Save and Schedule Job".'},
-                {imageURL: 'wise-help-images/BulkloaderClickThrough-07.png', text:'Select the calendar to choose a date.  Data will be pulled from your system starting from this date to the present time.'},
-                {imageURL: 'wise-help-images/BulkloaderClickThrough-08.png', text:'Click "Schedule Job".'},
-                {imageURL: 'wise-help-images/BulkloaderClickThrough-09.png', text:'Click "OK".  An email will notify you when the job has completed.'}
+                {imageURL: 'wise-help-images/BulkloaderClickThrough-01.png', steps:[{text:'Select a service, the source of your data.'}]},
+                {imageURL: 'wise-help-images/BulkloaderClickThrough-02.png', steps:[{text:'Enter your credentials for that service.'}]},
+                {imageURL: 'wise-help-images/BulkloaderClickThrough-03.png', steps:[{text:'Select an object to map from the source application.'}]},
+                {imageURL: 'wise-help-images/BulkloaderClickThrough-04.png', steps:[{text:'The button at the top will select all fields for the object.'}]},
+                {imageURL: 'wise-help-images/BulkloaderClickThrough-05.png', steps:[{text:'Or select the fields you wish to map.'}]},
+                {imageURL: 'wise-help-images/BulkloaderClickThrough-06.png', steps:[{text:'Click "Save and Schedule Job".'}]},
+                {imageURL: 'wise-help-images/BulkloaderClickThrough-07.png', steps:[{text:'Select the calendar to choose a date.  Data will be pulled from your system starting from this date to the present time.'}]},
+                {imageURL: 'wise-help-images/BulkloaderClickThrough-08.png', steps:[{text:'Click "Schedule Job".'}]},
+                {imageURL: 'wise-help-images/BulkloaderClickThrough-09.png', steps:[{text:'Click "OK".  An email will notify you when the job has completed.'}]}
             ]
         }
 
